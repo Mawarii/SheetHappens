58b46be350c4a8c060620987fc08de2e0554c89a
Mawarii <8413207-Mawarii@users.noreply.gitlab.com>
2025-08-24 19:29:59 +0200

    feat: add saveCharacter function

--- frontend/app/pages/characters/[id].vue
+++ frontend/app/pages/characters/[id].vue
@@ -11,20 +11,43 @@ interface Character {
   data: any
 }
 
-const { data: character, pending, error } = await useFetch<Character>(`${api}/characters/${route.params.id}`, {
+const { data: character, pending } = await useFetch<Character>(`${api}/characters/${route.params.id}`, {
   headers,
   method: "GET",
   credentials: "include",
 })
+
+const edit = ref(false)
+
+const saveCharacter = async () => {
+  await $fetch(`${api}/characters/${route.params.id}`, {
+    headers,
+    method: "PUT",
+    credentials: "include",
+    body: JSON.stringify(character.value),
+  })
+}
 </script>
 
 <template>
   <div v-if="character">
     <h1>{{ character.name }}</h1>
+    <USwitch
+      v-model="edit"
+      label="Edit"
+    />
     <div v-if="pending">Loading...</div>
-    <div v-else-if="error">Error loading data</div>
     <div v-else>
-      <DynamicRenderer :data="character.data" />
+      <DynamicRenderer
+        :data="character.data"
+        :editable="edit"
+      />
+      <UButton
+        v-if="edit"
+        @click="saveCharacter"
+      >
+        Save
+      </UButton>
     </div>
   </div>
 </template>
