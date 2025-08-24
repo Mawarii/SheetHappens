dc1a3ec974e9087bab7db6ae3f0f67d3165e4de1
Mawarii <8413207-Mawarii@users.noreply.gitlab.com>
2025-08-24 19:29:24 +0200

    feat: add simple navbar

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,5 +1,28 @@
+<script setup lang="ts">
+interface User {
+  user_id: number
+  username: string
+  display_name: string
+}
+
+const user = useState<User>("user")
+</script>
+
 <template>
-  <NuxtLayout>
+  <nav class="px-4 py-3 flex items-center space-x-8">
+    <div class="text-lg font-bold">
+      SheetHappens
+    </div>
+    <ul class="flex space-x-4">
+      <li>
+        <NuxtLink to="/characters">Characters</NuxtLink>
+      </li>
+      <li v-if="user">
+        <NuxtLink to="/logout">Logout</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+  <UApp>
     <NuxtPage />
-  </NuxtLayout>
+  </UApp>
 </template>
